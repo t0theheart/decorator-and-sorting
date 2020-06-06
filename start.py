5cfca39575a1f0ee7b958fc7882ad85b4d0cb9d8
t0theheart <35728736+t0theheart@users.noreply.github.com>
2020-06-06 10:11:19 +0300

    create start.py

---
+++ start.py
@@ -0,0 +1,49 @@
+from decorator_and_sorting.program import Program
+from decorator_and_sorting.decorators import BubbleSortDecorator, SelectionSortDecorator, InsertionSortDecorator
+from enum import Enum
+
+
+class Sorting(Enum):
+    bubble = 'bubble'
+    selection = 'selection'
+    insertion = 'insertion'
+
+
+def main():
+    """
+        Example of using
+
+        Sorting program started!
+        Sort types: "bubble", "selection", "insertion"
+        Enter type sort: bubble
+        Enter read from: array.txt
+        Enter write to: result.txt
+        Sorting program end.
+    """
+
+    program = Program()
+
+    print('Sorting program started!')
+    print(f'Sort types: "{Sorting.bubble.value}", "{Sorting.selection.value}", "{Sorting.insertion.value}"')
+
+    sort_type = input('Enter type sort: ')
+    read_from = input('Enter read from: ')
+    write_to = input('Enter write to: ')
+
+    program.read(read_from)
+
+    if sort_type == Sorting.bubble.value:
+        BubbleSortDecorator(program).write(write_to)
+    elif sort_type == Sorting.selection.value:
+        SelectionSortDecorator(program).write(write_to)
+    elif sort_type == Sorting.insertion.value:
+        InsertionSortDecorator(program).write(write_to)
+    else:
+        print(f'Sorting program does not support "{sort_type}" sorting')
+        program.write(write_to)
+
+    print('Sorting program end.')
+
+
+if __name__ == '__main__':
+    main()
